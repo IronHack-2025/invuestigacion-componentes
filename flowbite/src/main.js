7168b24f2ff2b6903e58c81f7f0f429c9c276582
omiras <oscar.miras@gmail.com>
2025-08-27 21:52:33 +0200

    wip

---
+++ flowbite/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// para que dropdowns, modals, etc. funcionen
+import 'flowbite'
+
+
+createApp(App).mount('#app')
